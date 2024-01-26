def8771b64a6be409733082ce039643821d66e23
thantzawaung023 <thantzinaung023@gmail.com>
2024-01-26 12:10:27 +0630

    [Complete] Rest Api Blog Update,Patch,Delete

--- TZADotNetCore.RestApi/Controllers/BlogController.cs
+++ TZADotNetCore.RestApi/Controllers/BlogController.cs
@@ -4,6 +4,7 @@ using TZADotNetCore.RestApi.Models;
 
 namespace TZADotNetCore.RestApi.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class BlogController : ControllerBase
@@ -18,19 +19,19 @@ namespace TZADotNetCore.RestApi.Controllers
         }
 
         [HttpGet("{pageNo}/{pageSize}")]
-        public IActionResult GetBlogs(int pageSize,int pageNo)
+        public IActionResult GetBlogs(int pageSize, int pageNo)
         {
 
             // end row no = pageNO * pageSize ; 10
             //start row no = end row - page size +1 ;
-            List<BlogDataModel> lst = _dbContext.Blogs.Skip((pageNo-1)*pageSize).Take(pageSize).ToList();
+            List<BlogDataModel> lst = _dbContext.Blogs.Skip((pageNo - 1) * pageSize).Take(pageSize).ToList();
             var rowCount = _dbContext.Blogs.Count();
-            var pageCount = rowCount/ pageSize;
+            var pageCount = rowCount / pageSize;
             if (rowCount % pageSize > 0)
             {
                 pageCount++;
             }
-            return Ok(new {IsEndOfPage = pageCount <= pageNo,PageCount = pageCount, PageSize = pageSize,PageNo = pageNo,Data = lst});
+            return Ok(new { IsEndOfPage = pageCount <= pageNo, PageCount = pageCount, PageSize = pageSize, PageNo = pageNo, Data = lst });
         }
 
         [HttpGet("{id}")]
@@ -39,7 +40,6 @@ namespace TZADotNetCore.RestApi.Controllers
             BlogDataModel item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
             if (item is null)
             {
-
                 return NotFound("No Data Found.");
             }
             return Ok(item);
@@ -54,24 +54,76 @@ namespace TZADotNetCore.RestApi.Controllers
             return Ok(message);
         }
 
-        [HttpPut]
-        public IActionResult UpdateBlog()
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, BlogDataModel blog)
         {
-            return Ok("Update");
+            BlogDataModel item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+
+            if (string.IsNullOrEmpty(blog.Blog_Title))
+            {
+                return BadRequest("Blog_Title is required.");
+            }
+            if (string.IsNullOrEmpty(blog.Blog_Author))
+            {
+                return BadRequest("Blog_Author is required.");
+            }
+            if (string.IsNullOrEmpty(blog.Blog_Content))
+            {
+                return BadRequest("Blog_Content is required.");
+            }
+
+            int result = _dbContext.SaveChanges();
+            var message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            return Ok(message);
         }
 
 
-        [HttpPatch]
-        public IActionResult PatchBlog()
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, BlogDataModel blog)
         {
-            return Ok("Patch");
+            BlogDataModel item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            if (!string.IsNullOrEmpty(blog.Blog_Title))
+            {
+                item.Blog_Title = blog.Blog_Title;
+            }
+            if (!string.IsNullOrEmpty(blog.Blog_Author))
+            {
+                item.Blog_Author = blog.Blog_Title;
+            }
+            if (!string.IsNullOrEmpty(blog.Blog_Content))
+            {
+                item.Blog_Content = blog.Blog_Content;
+            }
+
+            int result = _dbContext.SaveChanges();
+            var message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            return Ok(message);
         }
 
 
-        [HttpDelete]
-        public IActionResult DeleteBlog()
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
         {
-            return Ok("delete");
+            BlogDataModel item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            _dbContext.Blogs.Remove(item);
+            int result = _dbContext.SaveChanges();
+            var message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            return Ok(message);
         }
 
     }
--- TZADotNetCore.RestApi/Models/BlogDataModel.cs
+++ TZADotNetCore.RestApi/Models/BlogDataModel.cs
@@ -13,9 +13,9 @@ namespace TZADotNetCore.RestApi.Models
     {
         [Key]
         public int Blog_Id { get; set; }
-        public string Blog_Title { get; set; }
-        public string Blog_Author { get; set; }
-        public string Blog_Content { get; set; }
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
 
     }
 }
