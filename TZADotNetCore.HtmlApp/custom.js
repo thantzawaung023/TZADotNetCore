df3510664458ae36191f2ab36ef2f645839f4da7
thantzawaung023 <thantzinaung023@gmail.com>
2024-01-29 19:58:24 +0630

    [Complete] Notiflix, DataTable ,Ladda Button, Icheck

--- TZADotNetCore.HtmlApp/custom.js
+++ TZADotNetCore.HtmlApp/custom.js
@@ -1,24 +1,39 @@
 var successMessage = (message) => {
-  Swal.fire({
-    icon: "success",
-    title: message,
-    showConfirmButton: false,
-    timer: 1500,
-  });
+  //   Swal.fire({
+  //     icon: "success",
+  //     title: message,
+  //     showConfirmButton: false,
+  //     timer: 1500,
+  //   });
+  Notiflix.Report.success("Notiflix Success", message, "Okay");
 };
 
 var comfirmMessage = (message) => {
+  //   return new Promise(function (myResolve, myReject) {
+  //     Swal.fire({
+  //       title: "Comfirm",
+  //       text: message,
+  //       icon: "warning",
+  //       showCancelButton: true,
+  //       confirmButtonColor: "#3085d6",
+  //       cancelButtonColor: "#d33",
+  //       confirmButtonText: "Yes",
+  //     }).then((result) => {
+  //       return myResolve(result.isConfirmed);
+  //     });
+  //   });
   return new Promise(function (myResolve, myReject) {
-    Swal.fire({
-      title: "Comfirm",
-      text: message,
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes",
-    }).then((result) => {
-      return myResolve(result.isConfirmed);
-    });
+    Notiflix.Confirm.show(
+      "Confirm",
+      message,
+      "Yes",
+      "No",
+      function okCb() {
+        myResolve(true);
+      },
+      function cancelCb() {
+        myResolve(false);
+      }
+    );
   });
 };
